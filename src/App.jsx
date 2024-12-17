e4824fff2280d9ed813d2303d7c0135beaa8a50b
VictorOdongo <victorodongo397@gmail.com>
2024-12-17 13:57:53 +0300

    Footer section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import HeroSection from "./components/HeroSection";
-import Navbar from "./components/Navbar"
+import Navbar from "./components/Navbar";
 import FeatureSection from "./components/FeatureSection";
 import Workflow from "./components/Workflow";
 import Pricing from "./components/Pricing";
 import Testimonials from "./components/Testimonials";
+import Footer from "./components/Footer"; 
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
         <Workflow />
         <Pricing />
         <Testimonials />
+        <Footer />
       </div>
       
     </>
