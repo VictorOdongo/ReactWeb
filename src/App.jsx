3b233175ac6c9e56125d2eba4682f509255be490
VictorOdongo <victorodongo397@gmail.com>
2024-12-17 12:47:51 +0300

    Testimonials

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar"
 import FeatureSection from "./components/FeatureSection";
 import Workflow from "./components/Workflow";
+import Pricing from "./components/Pricing";
+import Testimonials from "./components/Testimonials";
 
 const App = () => {
   return (
@@ -11,6 +13,8 @@ const App = () => {
         <HeroSection />
         <FeatureSection />
         <Workflow />
+        <Pricing />
+        <Testimonials />
       </div>
       
     </>
